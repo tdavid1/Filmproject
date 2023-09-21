d1aa0606b4bbc74e6a7b5de4758d537663e561fd
TomikDavid <tomik.david1@gmail.com>
2023-09-21 11:17:39 +0200

    2.0

---
+++ Filmproject/Film.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmproject
+{
+    internal class Film
+    {
+        private int helyezes;
+        private string cim;
+        private int ev;
+        private int hosszu;
+        private List<string> mufaj;
+
+        public Film(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            this.helyezes = int.Parse(adatok[0].Substring(0, adatok[0].Length-1));
+            this.cim = adatok[1];
+            this.ev = int.Parse(adatok[2]);
+            this.hosszu = int.Parse(adatok[3].Split(' ')[0]);
+            this.mufaj = new List<string>();
+            string[] mufajok = adatok[4].Split(',');
+            foreach(var item in mufajok)
+            {
+                this.mufaj.Add(item.Trim());
+            }
+        }
+        public Film(int helyezes, string cim, int ev, int hosszu, List<string> mufaj)
+        {
+            this.helyezes = helyezes;
+            this.cim = cim;
+            this.ev = ev;
+            this.hosszu = hosszu;
+            this.mufaj = mufaj;
+        }
+
+        public int Helyezes { get => helyezes; set => helyezes = value; }
+        public string Cim { get => cim; set => cim = value; }
+        public int Ev { get => ev; set => ev = value; }
+        public int Hosszu { get => hosszu; set => hosszu = value; }
+        public List<string> Mufaj { get => mufaj; set => mufaj = value; }
+    }
+}
--- Filmproject/Program.cs
+++ Filmproject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,112 @@ namespace Filmproject
 {
     internal class Program
     {
+        static List<Film>filmek = new List<Film>();
         static void Main(string[] args)
         {
+            Fajbeolvasas("filmadatbazis.csv");
+        }
+        private static void f4()
+        {
+            string keresendo = Console.ReadLine();
+            List<string> cimek = filmkeres(keresendo);
+            foreach (string item in cimek)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static List<string> filmkeres(string keresendo)
+        {
+            List<string> list = new List<string>();
+            foreach(var item in filmek)
+            {
+                if (item.Cim.Contains(keresendo))
+                {
+                    list.Add(item.Cim);
+                }
+            }
+
+            return list;
+        }
+
+        private static void f3()
+        {
+            string cim = Console.ReadLine();
+            Film film = filmkers(cim);
+            if(film == null)
+            {
+                Console.WriteLine("A megadott film nem található");
+            }
+            else
+            {
+                Console.WriteLine($"A megadott film {film.Hosszu} perces");
+            }
+        }
+
+        private static Film filmkers(string cim)
+        {
+            int i = 0;
+            while (i<filmek.Count && filmek[i].Cim != cim)
+            {
+                i++;
+            }
+            if (i < filmek.Count)
+            {
+                return filmek[i];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static void f1()
+        {
+            Console.WriteLine($"1. A filmek átlagos hossza: {getalenghthossz()}perc");
+        }
+        private static void f2() 
+        {
+            Film leghoszabbfilm = getleghosszabbfilm();
+            Console.WriteLine($"2. A leghosszabb film Címe:{leghoszabbfilm.Cim} Hossza:{leghoszabbfilm.Hosszu}");
+        }
+
+        private static Film getleghosszabbfilm()
+        {
+            Film leghosszabb = filmek[0];
+            for (int i = 1; i < filmek.Count; i++)
+            {
+                if (filmek[i].Hosszu > leghosszabb.Hosszu)
+                {
+                    leghosszabb = filmek[i];
+                }
+            }
+            return leghosszabb;
+        }
+
+        private static object getalenghthossz()
+        {
+            int oszhoz=0;
+            foreach (var item in filmek)
+            {
+                oszhoz += item.Hosszu;
+
+            }
+            return oszhoz/filmek.Count;
+        }
+
+        private static void Fajbeolvasas(string file)
+        {
+            using(StreamReader r = new StreamReader(file))
+            {
+                r.ReadLine();
+                while (r.EndOfStream)
+                {
+                    string sor = r.ReadLine();
+                    Film film = new Film(sor);
+                    filmek.Add(film);
+                }
+            }
         }
     }
 }
